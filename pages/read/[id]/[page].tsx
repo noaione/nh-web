18faf29030bd116e73c4d6dfb2455da17fcea876
noaione <noaione0809@gmail.com>
2021-06-04 18:07:04 +0700

    pages: add route for reading section

---
+++ pages/read/[id]/[page].tsx
@@ -0,0 +1,251 @@
+import React from "react";
+import ErrorPage from "next/error";
+import Head from "next/head";
+import Link from "next/link";
+import Router from "next/router";
+import { GetStaticPropsContext } from "next";
+
+import Layout from "../../../components/Layout";
+import ReaderComponent from "../../../components/Reader";
+
+import { nhImage, nhInfoRawResult, nhInfoResult, nhTitle, RawGQLData } from "../../../lib/types";
+
+import { queryFetch } from "../../../lib/api";
+import { isNone, walk } from "../../../lib/utils";
+
+const ImageReaderSchemas = `query ReaderQuery($id:ID!) {
+    nhentai {
+        info(doujin_id:$id) {
+            id
+            title {
+                simple
+                english
+                japanese
+            }
+            cover_art {
+                type
+                url
+            }
+            images {
+                url
+            }
+            url
+            total_pages
+        }
+    }
+}`;
+
+interface ReaderImageProps {
+    image: nhImage;
+    id: string;
+    title: nhTitle;
+    cover_art: nhImage;
+    total_pages: number;
+    page: number;
+    notFound?: boolean;
+    realUrl: string;
+}
+
+export default class ReaderPerPagePages extends React.Component<ReaderImageProps> {
+    constructor(props) {
+        super(props);
+        this.state = {
+            downloaded: 0,
+            isCompleted: false,
+        };
+    }
+
+    async componentDidMount() {
+        const MAX_PREFETCH = 3;
+        const { id, page, total_pages, notFound } = this.props;
+        if (!notFound) {
+            console.info("Start prefetching...");
+            const prefetchPages = [];
+            if (page === 1) {
+                let addedCount = 0;
+                for (let i = 2; i < total_pages; i++) {
+                    if (addedCount >= MAX_PREFETCH) {
+                        break;
+                    }
+                    prefetchPages.push(i);
+                    addedCount++;
+                }
+            } else if (page === total_pages) {
+                let addedCount = 0;
+                for (let i = total_pages - 1; i > 0; i--) {
+                    if (addedCount >= MAX_PREFETCH) {
+                        break;
+                    }
+                    prefetchPages.push(i);
+                    addedCount++;
+                }
+            } else {
+                let addedCount = 0;
+                for (let i = page + 1; i < total_pages; i++) {
+                    if (addedCount >= MAX_PREFETCH) {
+                        break;
+                    }
+                    prefetchPages.push(i);
+                    addedCount++;
+                }
+                addedCount = 0;
+                for (let i = page - 1; i > 0; i--) {
+                    if (addedCount >= MAX_PREFETCH) {
+                        break;
+                    }
+                    prefetchPages.push(i);
+                    addedCount++;
+                }
+            }
+            prefetchPages.forEach((prePage) => {
+                console.info("[Prefetch]", "Page", prePage);
+                Router.prefetch(`/read/${id}/${prePage}`);
+            });
+        }
+    }
+
+    render() {
+        const { id, title, total_pages, cover_art, image, page, notFound } = this.props;
+        if (notFound) {
+            return <ErrorPage statusCode={404} />;
+        }
+        const { simple, japanese, english } = title;
+
+        const mainTitle = simple || english || japanese;
+        let realTitle = japanese || simple;
+        if (realTitle === mainTitle) {
+            realTitle = english || simple;
+        }
+
+        let urlNext = `/read/${id}/`;
+        if (page === 1 && page + 1 === total_pages) {
+            urlNext = "#";
+        } else if (page + 1 === total_pages) {
+            urlNext = "#";
+        } else if (page + 1 < total_pages) {
+            urlNext = (page + 1).toString();
+        }
+
+        return (
+            <>
+                <Head>
+                    <title>{mainTitle} :: nhProxy</title>
+                </Head>
+                <Layout
+                    title={mainTitle + " - Read"}
+                    description="Read Doujin from nHentai without blocking, made for naoTimes"
+                    mode="reader"
+                    urlPath={"/read/" + id}
+                    image={cover_art.url}
+                    mainClassName="mx-0"
+                >
+                    <div className="flex-1 overflow-x-hidden overflow-y-auto py-6">
+                        <ReaderComponent.Container className="mt-4 mb-6 mx-0 px-0 py-0 justify-center w-full max-w-full flex-0 bg-gray-900">
+                            {/* Set controls here */}
+                            <div
+                                className="flex justify-center overflow-x-auto overflow-y-visible max-w-full h-auto min-h-[300px]"
+                                aria-label="Image Container"
+                            >
+                                <Link href={urlNext} passHref>
+                                    <a onClick={(ev) => ev.preventDefault()}>
+                                        <img
+                                            src={image.url}
+                                            className="max-w-full h-full align-bottom select-none cursor-pointer"
+                                            onClick={(ev) => {
+                                                ev.preventDefault();
+                                                const halfWidth = ev.currentTarget.clientWidth / 2;
+                                                const { offsetX } = ev.nativeEvent;
+                                                const isRightSize = offsetX >= halfWidth;
+                                                if (!isRightSize) {
+                                                    // Dont go back anymore dumb dumb.
+                                                    if (page === 1) {
+                                                        return;
+                                                    } else {
+                                                        Router.push(`/read/${id}/${page - 1}`);
+                                                    }
+                                                } else {
+                                                    // Dont go forward anymore dumbass.
+                                                    if (page === total_pages) {
+                                                        return;
+                                                    } else {
+                                                        Router.push(`/read/${id}/${page + 1}`);
+                                                    }
+                                                }
+                                            }}
+                                        />
+                                    </a>
+                                </Link>
+                            </div>
+                            {/* Set controls here */}
+                        </ReaderComponent.Container>
+                    </div>
+                </Layout>
+            </>
+        );
+    }
+}
+
+export async function getStaticProps({ params }: GetStaticPropsContext) {
+    const { id, page } = params;
+    if (typeof id !== "string" || typeof page !== "string") {
+        return {
+            notFound: true,
+        };
+    }
+    const pageNo = parseInt(page);
+    if (isNaN(pageNo)) {
+        return {
+            notFound: true,
+        };
+    }
+
+    const selectFirst = Array.isArray(id) ? id[0] : id;
+
+    const apiResult = await queryFetch<RawGQLData<nhInfoRawResult>>(ImageReaderSchemas, {
+        id: selectFirst,
+    });
+    const rawData = walk<nhInfoResult>(apiResult, "data.nhentai.info");
+    if (isNone(rawData)) {
+        return {
+            notFound: true,
+        };
+    }
+
+    const { images, total_pages } = rawData;
+    if (images.length < 1) {
+        return {
+            notFound: true,
+        };
+    }
+    const selectPage = pageNo - 1;
+    if (selectPage < 0 || selectPage > total_pages - 1) {
+        return {
+            notFound: true,
+        };
+    }
+
+    const imgPage = images[pageNo - 1];
+    if (isNone(imgPage)) {
+        return {
+            notFound: true,
+        };
+    }
+
+    const results: ReaderImageProps = {
+        id: rawData.id,
+        image: imgPage,
+        title: rawData.title,
+        cover_art: rawData.cover_art,
+        realUrl: rawData.url,
+        total_pages: rawData.total_pages,
+        page: pageNo,
+    };
+
+    return {
+        props: results,
+    };
+}
+
+export async function getStaticPaths() {
+    return { paths: [], fallback: "blocking" };
+}
---
+++ pages/read/[id]/index.tsx
@@ -0,0 +1,201 @@
+import React from "react";
+import Head from "next/head";
+import { NextPageContext } from "next";
+
+import Layout from "../../../components/Layout";
+import ReaderComponent from "../../../components/Reader";
+
+import { nhInfoRawResult, nhInfoResult, RawGQLData } from "../../../lib/types";
+
+import { queryFetch } from "../../../lib/api";
+import { isNone, walk } from "../../../lib/utils";
+import ReactTimeago from "react-timeago";
+
+const ImageReaderSchemas = `query ReaderQuery($id:ID!) {
+    nhentai {
+        info(doujin_id:$id) {
+            id
+            media_id
+            title {
+                simple
+                english
+                japanese
+            }
+            cover_art {
+                type
+                url
+                original_url
+                sizes
+            }
+            tags {
+                artists {
+                    name
+                    amount
+                }
+                categories {
+                    name
+                    amount
+                }
+                groups {
+                    name
+                    amount
+                }
+                languages {
+                    name
+                    amount
+                }
+                tags {
+                    name
+                    amount
+                }
+                parodies {
+                    name
+                    amount
+                }
+                characters {
+                    name
+                    amount
+                }
+            }
+            images {
+                type
+                url
+                original_url
+                sizes
+            }
+            url
+            publishedAt
+            favorites
+            total_pages
+        }
+    }
+}`;
+
+interface DownloaderPagesProps {
+    data: nhInfoResult;
+}
+
+export default class ReaderPages extends React.Component<DownloaderPagesProps> {
+    static async getInitialProps({ query }: NextPageContext) {
+        const { id } = query;
+        if (typeof id !== "string") {
+            return {
+                notFound: true,
+            };
+        }
+
+        const selectFirst = Array.isArray(id) ? id[0] : id;
+
+        const res = await queryFetch<RawGQLData<nhInfoRawResult>>(ImageReaderSchemas, { id: selectFirst });
+        const rawData = walk<nhInfoResult>(res, "data.nhentai.info");
+        if (isNone(rawData)) {
+            return {
+                notFound: true,
+            };
+        }
+
+        return {
+            data: rawData,
+        };
+    }
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            downloaded: 0,
+            isCompleted: false,
+        };
+    }
+
+    render() {
+        const { id, title, total_pages, cover_art, images, tags, publishedAt } = this.props.data;
+        const { simple, japanese, english } = title;
+
+        const mainTitle = simple || english || japanese;
+        let realTitle = japanese || simple;
+        if (realTitle === mainTitle) {
+            realTitle = english || simple;
+        }
+        return (
+            <>
+                <Head>
+                    <title>{mainTitle} :: nhProxy</title>
+                </Head>
+                <Layout
+                    title={mainTitle + " - Read"}
+                    description="Read Doujin from nHentai without blocking, made for naoTimes"
+                    mode="reader"
+                    urlPath={"/read/" + id}
+                    image={cover_art.url}
+                >
+                    <div className="flex-1 overflow-x-hidden overflow-y-auto">
+                        <ReaderComponent.Container>
+                            <div className="grid grid-cols-1 lg:grid-cols-2 gap-2">
+                                <div className="flex justify-center items-center">
+                                    <img
+                                        className="object-cover h-[30rem] object-center"
+                                        src={cover_art.url}
+                                    />
+                                </div>
+                                <div className="flex flex-col mt-4 lg:mt-0">
+                                    <div className="text-2xl font-bold">{mainTitle}</div>
+                                    <div className="text-base font-semibold text-gray-300">{realTitle}</div>
+                                    <div className="mt-2 flex flex-col">
+                                        <ReaderComponent.TagGroup groupName="Parodies" tags={tags.parodies} />
+                                        <ReaderComponent.TagGroup
+                                            groupName="Characters"
+                                            tags={tags.characters}
+                                        />
+                                        <ReaderComponent.TagGroup groupName="Tags" tags={tags.tags} />
+                                        <ReaderComponent.TagGroup groupName="Artists" tags={tags.artists} />
+                                        <ReaderComponent.TagGroup groupName="Groups" tags={tags.groups} />
+                                        <ReaderComponent.TagGroup
+                                            groupName="Languages"
+                                            tags={tags.languages}
+                                        />
+                                        <ReaderComponent.TagGroup
+                                            groupName="Categories"
+                                            tags={tags.categories}
+                                        />
+                                        <span>
+                                            <span className="font-semibold">Pages:</span>
+                                            <ReaderComponent.Tag name={total_pages.toString()} />
+                                        </span>
+                                        <span className="gap-1 flex flex-row">
+                                            <span className="font-semibold">Uploaded:</span>
+                                            <ReactTimeago date={new Date(publishedAt)} />
+                                        </span>
+                                        <span
+                                            className="flex flex-col mt-4 cursor-pointer"
+                                            onClick={() => {
+                                                navigator.clipboard.writeText(id);
+                                            }}
+                                        >
+                                            <span className="font-mono text-gray-400 tracking-wide">
+                                                {"#" + id}
+                                            </span>
+                                        </span>
+                                    </div>
+                                </div>
+                            </div>
+                        </ReaderComponent.Container>
+                        <ReaderComponent.Container className="mt-4 mb-6">
+                            <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-2 md:gap-4 items-start">
+                                {images.map((img, idx) => {
+                                    return (
+                                        <ReaderComponent.Thumbnail
+                                            key={`thumb-${id}-p${idx + 1}`}
+                                            src={img.url}
+                                            doujinId={id}
+                                            pageNo={idx + 1}
+                                        />
+                                    );
+                                })}
+                            </div>
+                        </ReaderComponent.Container>
+                    </div>
+                </Layout>
+            </>
+        );
+    }
+}
